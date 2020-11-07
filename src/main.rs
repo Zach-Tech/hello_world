e1268507a7657ddfe2a8ced60729f785d9e482b4
Zachary Childers <zdchilders10@sccsc.edu>
2020-11-07 12:20:03 -0500

    just doing a loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut count=0u32;
+    println!("Hello, world - to Infinity and Beyond!"); //I think this is an inf. loop
+    loop{
+        count +=1;
+        if count == 24{
+            println!("wanna know what's funnier than 24?");
+            continue
+        }
+        if count == 25{
+            println!("25....");
+            continue
+        }
+
+        println!("{}",count);
+        if count == 35{
+            print!("k bye bby luv u");
+            break;
+        }
+    }
+
 }
